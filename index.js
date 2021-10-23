1814498800ab9161befce5758fe2c2e0f35de9b9
TakiKauser <takikauserbadhon@gmail.com>
2021-10-23 14:58:35 +0600

    DELETE-API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient } = require('mongodb');
 const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,13 +26,23 @@ async function run() {
         app.get('/users', async (req, res) => {
             const users = await usersCollection.find({}).toArray();
             res.send(users);
-        })
+        });
+
         // POST API
         app.post('/users', async (req, res) => {
             const newUser = req.body;
             result = await usersCollection.insertOne(newUser);
             res.json(result);
+        });
+
+        // DELETE API
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.json(result);
         })
+
     } finally {
         // await client.close();
     }
